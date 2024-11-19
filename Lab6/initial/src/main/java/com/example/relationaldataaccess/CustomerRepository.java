dcb29a9d6e165229f8eb30a2a75824e412dbb3e9
CarinaChen423 <chaselc011@gmail.com>
2024-11-19 23:42:04 +0100

    19.11 show results in RESTful service

--- Lab6/initial/src/main/java/com/example/relationaldataaccess/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.relationaldataaccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // Spring Data JPA provides basic CRUD methods by default
-}
