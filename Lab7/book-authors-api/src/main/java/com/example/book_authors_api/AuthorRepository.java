6058d1aaa0e2638b21037c087e4cada720fe233f
student <chaselc011@gmail.com>
2024-11-27 13:55:38 +0100

    27,11

--- Lab7/book-authors-api/src/main/java/com/example/book_authors_api/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.book_authors_api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
---
+++ chen/src/main/java/bookauthor/chen/Book.java
@@ -0,0 +1,4 @@
+package bookauthor.chen;
+
+public class Book {
+}
